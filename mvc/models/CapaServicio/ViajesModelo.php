db6b332ce512e8181dca214962d66460d9b62cbf
Alejandro Ezequiel Mendez <mendezalejandro.e@gmail.com>
2016-10-23 11:33:53 -0300

    Se agrega ViajesModelo

--- mvc/models/CapaServicio/ViajesModelo.php
+++ mvc/models/CapaServicio/ViajesModelo.php
@@ -15,7 +15,7 @@ use yii\base\Model;
 class ViajesModelo extends Model
 {
     const Operacion_Alta = 0, Operacion_Modificacion=1, Operacion_Baja=2, Operacion_GetInfo=3;  //CONSTANTES QUE SIRVEN PARA UTILIZARLAS COMO REFERENCIA A CADA OPERACION.
-    const spABM='Viajes_ABM', spGetInfo='Viajes_GetInfo';                                   //NOMBRES DE STORED PROCEDURES QUE SE UTILIZARAN EN EL MODELO.
+    const spABM='Viaje_ABM', spGetInfo='Viaje_GetInfo';                                   //NOMBRES DE STORED PROCEDURES QUE SE UTILIZARAN EN EL MODELO.
     private $Operacion=null;                                                                    //VARIABLE AUXILIAR PARA GUARDAR LA CONSTANTE DE OPERACION QUE SE UTILIZARA COMO REFERENCIA.
     private $OperacionState=null;                                                               //VARIABLE QUE GUARDARA EL ESTADO DE LA OPERACION ACTUAL (ALTA, BAJA, MODIFICACION o GETINFO).
     private $Viajes=null;                                                                     //VARIABLE QUE SERVIRA PARA GUARDAR LAS ViajeS QUE DEVUELVA EL METODO GETINFO.
@@ -23,23 +23,27 @@ class ViajesModelo extends Model
 
 
 
-    public function RegistrarViaje($Nombre, $Apellido,$Usuario,$Password, $Telefono, $Email, $Direccion, $DireccionCoordenadas, $DireccionDefault, $Estado, $RolID, $Documento, $AgenciaID)                                               //ESTE METODO RECIBE UN Lista COMO PARAMETRO, LA Lista DEBE CONTENER LA MISMA CANTIDAD DE PARAMETROS QUE SE UTILIZAN EN EL STORE PROCEDURE CON LOS MISMOS NOMBRES EXCEPTUANDO LOS PARAMETROS (operacion, ViajeID y @result).
+    public function RegistrarViaje($ChoferID, $VehiculoID,$TarifaID, $TurnoID, $AgenciaID, $PersonaID, $FechaEmision,$FechaSalida, $ViajeTipo, $OrigenCoordenadas, $DestinoCoordenadas, $OrigenDireccion, $DestinoDireccion, $Comentario, $ImporteTotal, $Distancia, $Estado)                                               //ESTE METODO RECIBE UN Lista COMO PARAMETRO, LA Lista DEBE CONTENER LA MISMA CANTIDAD DE PARAMETROS QUE SE UTILIZAN EN EL STORE PROCEDURE CON LOS MISMOS NOMBRES EXCEPTUANDO LOS PARAMETROS (operacion, ViajeID y @result).
     {
         $this->Parametros = [
-                'Nombre' => $Nombre,
-                'Apellido' => $Apellido,
-                'Usuario' => $Usuario,
-                'Password' => $Password,
-                'Telefono' => $Telefono,
-                'Email' => $Email,
-                'Direccion' => $Direccion,
-                'DireccionCoordenadas' => $DireccionCoordenadas,
-                'DireccionDefault' => $DireccionDefault,
-                'Estado' =>$Estado,
-                'RolID' =>$RolID,
-                'Documento' =>$Documento,
-                'AgenciaID' =>$AgenciaID,
-                ];
+    'ChoferID'=> $ChoferID,
+    'VehiculoID'=> $VehiculoID,
+    'TarifaID'=> $TarifaID,
+    'TurnoID'=> $TurnoID,
+    'AgenciaID'=> $AgenciaID,
+    'PersonaID'=> $PersonaID,
+    'FechaEmision'=> $FechaEmision,
+    'FechaSalida'=> $FechaSalida,
+    'ViajeTipo'=> $ViajeTipo,
+    'OrigenCoordenadas'=> $OrigenCoordenadas,
+    'DestinoCoordenadas'=> $DestinoCoordenadas,
+    'OrigenDireccion'=> $OrigenDireccion,
+    'DestinoDireccion'=> $DestinoDireccion,
+    'Comentario'=> $Comentario,
+    'ImporteTotal'=> $ImporteTotal,
+    'Distancia'=> $Distancia,
+    'Estado'=> $Estado
+      ];
         $this->setOperacion(self::Operacion_Alta);                                                      //LLAMA AL METODO setOperacion y SETEA LA VARIABLE $operacionState CON UN OBJETO Alta() DE LA CLASE OPERACIONES.
         $this->Viajes = $this->OperacionState->EjecutarOperacion($this->Parametros,self::spABM);      //EJECUTA EL METODO EjecutarOperacion() DEL OBJETO OperacionState (un objeto Alta()) Y LE PASA COMO PARAMETROS LA LISTA DE PARAMETROS Y LA CONSTANTE CON EL NOMBRE DEL STORED PROCEDURE DE ABM. GUARDA LA INFORMACION QUE DEVUELVE EN LA VARIABLE $Viajes QUE SERA UNA LISTA CON UN SOLO VALOR ($id de la Viaje insertada).
         return $this->Viajes;
@@ -47,64 +51,81 @@ class ViajesModelo extends Model
     public function EliminarViaje($ViajeID)
     {
         $this->Parametros = [
-                'ViajeID' => $ViajeID,
-                'Nombre' => "",
-                'Apellido' => "",
-                'Usuario' => "",
-                'Password' => "",
-                'Telefono' => "",
-                'Email' => "",
-                'Direccion' => "",
-                'DireccionCoordenadas' => "",
-                'DireccionDefault' => "",
-                'Estado' =>"",
-                'RolID' =>"",
-                'Documento' =>"",
-                'AgenciaID' =>"",
-                ];
+    'ViajeID'=> $ViajeID,
+    'ChoferID'=> "",
+    'VehiculoID'=> "",
+    'TarifaID'=> "",
+    'TurnoID'=> "",
+    'AgenciaID'=> "",
+    'PersonaID'=> "",
+    'FechaEmision'=> "",
+    'FechaSalida'=> "",
+    'ViajeTipo'=> "",
+    'OrigenCoordenadas'=> "",
+    'DestinoCoordenadas'=> "",
+    'OrigenDireccion'=> "",
+    'DestinoDireccion'=> "",
+    'Comentario'=> "",
+    'ImporteTotal'=> "",
+    'Distancia'=> "",
+    'Estado'=> ""
+      ];
         $this->setOperacion(self::Operacion_Baja);                                                    //LLAMA AL METODO setOperacion y SETEA LA VARIABLE $operacionState CON UN OBJETO Baja() DE LA CLASE OPERACIONES.
         $this->Viajes = $this->OperacionState->EjecutarOperacion($this->Parametros,self::spABM);    //EJECUTA EL METODO EjecutarOperacion() DEL OBJETO OperacionState (un objeto Baja()) Y LE PASA COMO PARAMETROS LA LISTA DE PARAMETROS Y LA CONSTANTE CON EL NOMBRE DEL STORED PROCEDURE DE ABM. GUARDA LA INFORMACION QUE DEVUELVE EN LA VARIABLE $Viajes QUE SERA UNA LISTA CON UN SOLO VALOR ($id de la Viaje eliminada).
 
         return $this->Viajes;
     }
-    public function ModificarViaje($ViajeID, $Nombre, $Apellido, $Usuario,$Password, $Telefono, $Email, $Direccion, $DireccionCoordenadas, $DireccionDefault, $DireccionTipo, $Estado, $RolID, $Documento, $AgenciaID)
+    public function ModificarViaje($ViajeID, $ChoferID, $VehiculoID,$TarifaID, $TurnoID, $AgenciaID, $PersonaID, $FechaEmision,$FechaSalida, $ViajeTipo, $OrigenCoordenadas, $DestinoCoordenadas, $OrigenDireccion, $DestinoDireccion, $Comentario, $ImporteTotal, $Distancia, $Estado)
     {
         $this->Parametros = [
-                'ViajeID' => $ViajeID,
-                'Nombre' => $Nombre,
-                'Apellido' => $Apellido,
-                'Usuario' => $Usuario,
-                'Password' => $Password,
-                'Telefono' => $Telefono,
-                'Email' => $Email,
-                'Direccion' => $Direccion,
-                'DireccionCoordenadas' => $DireccionCoordenadas,
-                'DireccionDefault' => $DireccionDefault,
-                'Estado' =>$Estado,
-                'RolID' =>$RolID,
-                'Documento' =>$Documento,
-                'AgenciaID' =>$AgenciaID,
-                ];
+            'ViajeID'=> $ViajeID,
+            'ChoferID'=> $ChoferID,
+            'VehiculoID'=> $VehiculoID,
+            'TarifaID'=> $TarifaID,
+            'TurnoID'=> $TurnoID,
+            'AgenciaID'=> $AgenciaID,
+            'PersonaID'=> $PersonaID,
+            'FechaEmision'=> $FechaEmision,
+            'FechaSalida'=> $FechaSalida,
+            'ViajeTipo'=> $ViajeTipo,
+            'OrigenCoordenadas'=> $OrigenCoordenadas,
+            'DestinoCoordenadas'=> $DestinoCoordenadas,
+            'OrigenDireccion'=> $OrigenDireccion,
+            'DestinoDireccion'=> $DestinoDireccion,
+            'Comentario'=> $Comentario,
+            'ImporteTotal'=> $ImporteTotal,
+            'Distancia'=> $Distancia,
+            'Estado'=> $Estado
+        ];
         $this->setOperacion(self::Operacion_Modificacion);                                              //LLAMA AL METODO setOperacion y SETEA LA VARIABLE $operacionState CON UN OBJETO Modificacion() DE LA CLASE OPERACIONES.
         $this->Viajes = $this->OperacionState->EjecutarOperacion($this->Parametros,self::spABM);      //EJECUTA EL METODO EjecutarOperacion() DEL OBJETO OperacionState (un objeto Modifcacion()) Y LE PASA COMO PARAMETROS LA LISTA DE PARAMETROS Y LA CONSTANTE CON EL NOMBRE DEL STORED PROCEDURE DE ABM. GUARDA LA INFORMACION QUE DEVUELVE EN LA VARIABLE $Viajes QUE SERA UNA LISTA CON UN SOLO VALOR ($id de la Viaje modificada).
         return $this->Viajes;
     }
-    public function GetInfoViajes($ViajeID, $Nombre, $Apellido,$Usuario, $Telefono, $Email, $Direccion, $DireccionCoordenadas, $Estado, $RolID, $Documento, $AgenciaID)
+    public function GetInfoViajes($ViajeID, $ChoferID, $VehiculoID,$TarifaID, $TurnoID, $AgenciaID, $PersonaID, $FechaEmisionDesde, $FechaEmisionHasta, $FechaSalidaDesde, $FechaSalidaHasta, $ViajeTipo, $OrigenCoordenadas, $DestinoCoordenadas, $OrigenDireccion, $DestinoDireccion, $Comentario, $ImporteTotal, $Distancia, $Estado)
     {
         $this->Parametros = [
-                'ViajeID' => $ViajeID,
-                'Nombre' => $Nombre,
-                'Apellido' => $Apellido,
-                'Usuario' => $Usuario,
-                'Telefono' => $Telefono,
-                'Email' => $Email,
-                'Direccion' => $Direccion,
-                'DireccionCoordenadas' => $DireccionCoordenadas,
-                'Estado' =>$Estado,
-                'RolID' =>$RolID,
-                'Documento' =>$Documento,
-                'AgenciaID' =>$AgenciaID,
-                ];
+            'ViajeID'=> $ViajeID,
+            'ChoferID'=> $ChoferID,
+            'VehiculoID'=> $VehiculoID,
+            'TarifaID'=> $TarifaID,
+            'TurnoID'=> $TurnoID,
+            'AgenciaID'=> $AgenciaID,
+            'PersonaID'=> $PersonaID,
+            'FechaEmisionDesde'=> $FechaEmisionDesde,
+            'FechaEmisionHasta'=> $FechaEmisionHasta,
+            'FechaSalidaDesde'=> $FechaSalidaDesde,
+            'FechaSalidaHasta'=> $FechaSalidaHasta,
+            'ViajeTipo'=> $ViajeTipo,
+            'OrigenCoordenadas'=> $OrigenCoordenadas,
+            'DestinoCoordenadas'=> $DestinoCoordenadas,
+            'OrigenDireccion'=> $OrigenDireccion,
+            'DestinoDireccion'=> $DestinoDireccion,
+            'Comentario'=> $Comentario,
+            'ImporteTotal'=> $ImporteTotal,
+            'Distancia'=> $Distancia,
+            'Estado'=> $Estado
+              ];
+
         $this->setOperacion(self::Operacion_GetInfo);                                                   //LLAMA AL METODO setOperacion y SETEA LA VARIABLE $operacionState CON UN OBJETO Getinfo() DE LA CLASE OPERACIONES.
         $this->Viajes = $this->OperacionState->EjecutarOperacion($this->Parametros,self::spGetInfo);  //EJECUTA EL METODO EjecutarOperacion() DEL OBJETO OperacionState (un objeto GetInfo()) Y LE PASA COMO PARAMETROS LA LISTA DE PARAMETROS Y LA CONSTANTE CON EL NOMBRE DEL STORED PROCEDURE DE GETINFO. GUARDA LA INFORMACION QUE DEVUELVE EN LA VARIABLE $Viajes QUE SERA UNA LISTA CON USUARIOS CUYOS VALORES SON LOS MISMOS QUE DEVUELVE EL STORED PROCEDURE.
         return $this->Viajes;                                                                         //SE RETORNA LA LISTA DE ViajeS.
@@ -135,19 +156,19 @@ class ViajesModelo extends Model
 /*
 $test = new ViajesModelo();
 TEST GET INFO
-print_r($test->GetInfoViajes(-1,"","","","","","","","",""));
+print_r($test->GetInfoViajes(-1,-1,-1,-1,-1,-1,-1,"","","","",-1,"","","","","",-1,-1,-1));
  */
 
 
 
 /*TEST REGISTRAR
-if($test->RegistrarViaje("'Son'","'Goku'","'ssj3'", "'ssj3'","'42134213'", "'supersaiyan@tierra.com'", "'Monta?a paos'","'{fjdsi}'",0,0,4)!=null) echo 'Cliente registrado correctamente!';
+if($test->RegistrarViaje(1,1,1,1,1,1,"'2010-01-01'","'2020-01-01'",0,"'{}'","'{}'","''","''","'nada'",100,20,0)!=null) echo 'Cliente registrado correctamente!';
  */
 
 /*TEST MODIFICACION
-if($test->ModificarViaje(72,"'Santino Alejandro'","'pempin'", "'pempin'","'2543654'", "'prueba@pepe.com'", "'Calle appsp'","'{fjdsi}'",0,0,4)!=null) echo 'Cliente modificado correctamente!';
+if($test->RegistrarViaje(4,2,2,1,1,1,1,"'2010-01-01'","'2020-01-01'",0,"'{}'","'{}'","''","''","'nada'",100,20,0)!=null) echo 'Cliente modificado correctamente!';
  */
 
 /*TEST ELIMINAR
-if($test->EliminarViaje(72)!=null) echo 'Cliente eliminado correctamente!';
+if($test->EliminarViaje(4)!=null) echo 'Cliente eliminado correctamente!';
  */
