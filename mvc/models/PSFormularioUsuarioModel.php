117c5f2291dd010b99be59ea7418d7c599a51b99
Alejandro Ezequiel Mendez <mendezalejandro.e@gmail.com>
2016-10-25 16:22:00 -0300

    Correccion Login y Registro

--- mvc/models/PSFormularioUsuarioModel.php
+++ mvc/models/PSFormularioUsuarioModel.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace app\models;
-
+include('CapaServicio/PersonasModelo.php');
 use yii\base\Model;
 
 class PSFormularioUsuarioModel extends Model {
 
     public $nombre;
     public $apellido;
+    public $documento;
     public $correo;
     public $telefono;
     public $direccion;
@@ -23,13 +24,15 @@ class PSFormularioUsuarioModel extends Model {
             ['nombre', 'required','message'=>'Campo obligatorio'],
             ['nombre', 'match','pattern'=>'/^[a-zA-Z ]*$/','message'=>'Ingrese solo letras'],
             ['nombre', 'match','pattern'=>'/^.{3,50}$/','message'=>'Ingrese como mínimo 3 y como máximo 50 letras'],
-            
+
             ['coordenadas', 'required'],
 
             ['apellido', 'required','message'=>'Campo obligatorio'],
             ['apellido', 'match','pattern'=>'/^[a-zA-Z ]*$/','message'=>'Ingrese solo letras'],
             ['apellido', 'match','pattern'=>'/^.{3,50}$/','message'=>'Ingrese como mínimo 3 y como máximo 50 letras'],
-            
+
+            ['documento', 'required','message'=>'Campo obligatorio'],
+            ['documento', 'match','pattern'=>'/^.{3,50}$/','message'=>'Ingrese como mínimo 8 y como máximo 15 caracteres'],
 
             ['correo', 'required','message'=>'Campo obligatorio'],
             //['correo', 'match','pattern'=>'/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$./','message'=>'Correo No Válido'],
@@ -40,8 +43,8 @@ class PSFormularioUsuarioModel extends Model {
             ['telefono', 'match','pattern'=>'/^[1-9]\d*$/','message'=>'Ingrese solo números'],
             ['telefono', 'match','pattern'=>'/^\d{8,20}/','message'=>'Ingrese como mínimo 8 y como máximo 20 números'],
 
-            ['direccion', 'required','message'=>'Campo obligatorio'],            
-            
+            ['direccion', 'required','message'=>'Campo obligatorio'],
+
             ['usuario', 'required','message'=>'Campo obligatorio'],
 
             ['contrasenia', 'required','message'=>'Campo obligatorio'],
@@ -54,7 +57,7 @@ class PSFormularioUsuarioModel extends Model {
     public function AltaRegistro()
     {
         $model = new PersonasModelo(); //crea un nuevo modelo de personamodelo
-        $model->RegistrarPersona("'$this->nombre'","'$this->apellido'","'$this->usuario'","'$this->contrasenia'","'$this->telefono'","'$this->correo'","'$this->direccion'","'$this->coordenadas'","'0'","'0'","'4'"); //genera el alta del usuario y lo guarda
+        $model->RegistrarPersona("'$this->nombre'","'$this->apellido'","'$this->usuario'","'$this->contrasenia'","'$this->telefono'","'$this->correo'","'$this->direccion'","'$this->coordenadas'","'0'","'0'","'4'","'$this->documento'","''"); //genera el alta del usuario y lo guarda
         return true;
     }
 }
