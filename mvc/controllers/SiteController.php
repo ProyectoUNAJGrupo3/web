0b59a86ca823e6442baae1fd74c4f5ac6b04a102
encina94 <encina.sebastian94@gmail.com>
2016-10-25 17:45:00 -0300

    commit de prueba

--- mvc/controllers/SiteController.php
+++ mvc/controllers/SiteController.php
@@ -1,24 +1,17 @@
 <?php
 
 namespace app\controllers;
-
 use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
 
+use app\models\ModelosHome\ContactForm;
+
 use app\models\TipoUsuario;
 use app\models\PSFormularioLoginModel;
 use app\models\PSFormularioUsuarioModel;
-
-use app\models\ModelosHome\ContactForm;
-
-use app\models\ModelosVehiculos\PSFormularioAltaVehiculoModel;
-use app\models\ModelosVehiculos\PSFormularioActualizacionVehiculoModel;
-
-use app\models\ModelosEmpleados\PSFormularioNuevoEmpleadoModel;
-use app\models\ModelosEmpleados\PSActualizacionDatosChoferModel;
-use app\models\ModelosEmpleados\PSActualizacionDatosRecepcionistaModel;
+use app\models\PSFormularioSolictudServicioUsuarioModel;
 
 class SiteController extends Controller {
 
@@ -125,7 +118,7 @@ class SiteController extends Controller {
 
     // funciones para las vistas dependiendo el tipo de usuario
     public function actionAdministrador() {
-        return $this->render('index');
+        return $this->redirect(['agencia/index']);
     }
 
     public function actionRecepcionista() {
@@ -216,28 +209,9 @@ class SiteController extends Controller {
         return $this->render("PSFormularioUsuario", ['model' => $model]);
     }
 
-    public function actionAlta_vehiculo_agencia() {
-        $model = new PSFormularioAltaVehiculoModel();
-        return $this->render("PSFormularioAltaVehiculo", ['model' => $model]);
+    public function actionAbrir_solictud_servicio() {
+        $model = new PSFormularioSolictudServicioUsuarioModel();
+        return $this->render("solicitarServicioFormulario", ['model' => $model]);
     }
 
-    public function actionActualizar_vehiculo_agencia() {
-        $model = new PSFormularioActualizacionVehiculoModel();
-        return $this->render("PSFormularioActualizacionVehiculo", ['model' => $model]);
-    }
-
-    public function actionAlta_empleado_agencia() {
-        $model = new PSFormularioNuevoEmpleadoModel();
-        return $this->render("PSFormularioNuevoEmpleado", ['model' => $model]);
-    }
-
-    public function actionAlta_datos_chofer() {
-        $model = new PSActualizacionDatosChoferModel();
-        return $this->render("PSActualizacionDatosChofer", ['model' => $model]);
-    }
-
-    public function actionAlta_datos_recepcionista() {
-        $model = new PSActualizacionDatosRecepcionistaModel();
-        return $this->render("PSActualizacionDatosRecepcionista", ['model' => $model]);
-    }
 }
