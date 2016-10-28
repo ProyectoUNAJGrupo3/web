8e68cea4591ae0a6b65aa2a260253a102e1b3701
mendezalejandro <mendezalejandro.e@gmail.com>
2016-10-27 22:24:16 -0300

    Update RecepcionistaController.php

--- mvc/controllers/RecepcionistaController.php
+++ mvc/controllers/RecepcionistaController.php
@@ -6,12 +6,33 @@ use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
-use app\models\Recepcionista\AltaViajeManualModel;
-use app\models\Recepcionista\ListaSolicitudesServicioModel;
+use app\models\TipoUsuario;
 
-class RecepcionistaController extends Controller {
+class RecepcionistaController extends Controller{
 
     public $layout = 'mainRecepcionista';                           //se asocia al layout predeterminado
+
+    public function behaviors() {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['index'], //solo debe aplicarse a las acciones login, logout , admin,recepcionista, chofer y cliente. Todas las demas acciones no estan sujetas al control de acceso
+                'rules' => [                              //reglas
+                        //el administrador tiene permisos sobre las siguientes acciones
+                        ['actions' => ['index'],
+                        'allow' => true,
+                        'roles' => ['@'], //El arroba es para el usuario autenticado
+                        'matchCallback' => function ($rule, $action) {                    //permite escribir la l?gica de comprobaci?n de acceso arbitraria, las paginas que se intentan acceder solo pueden ser permitidas si es un...
+                            return TipoUsuario::usuarioRecepcionista(Yii::$app->user->identity->RolID);
+                            //Llamada al m?todo que comprueba si es un administrador
+                            //Retorno el metodo del modelo que comprueba el tipo de usuario que es por el rol (1,2,3,4) etc y que devuelve true o false
+                        }
+                                               ]
+                    ]
+                ]
+             ];
+    }
+
 
     public function actions() {
         Yii::$app->errorHandler->errorAction = 'recepcionista/error';             //seteo la ruta de vista de error para que al llamar al ErrorAction no vaya a la del site sino a la de agencia,
@@ -26,17 +47,5 @@ class RecepcionistaController extends Controller {
     public function actionIndex() {                      //renderiza el index de la carpeta agencia dentro de views
         return $this->render('index');
     }
-
-    public function actionAlta_viaje_manual() {                      //renderiza el index de la carpeta agencia dentro de views
-        $model = new AltaViajeManualModel();
-        return $this->render("altaViajeManual", ['model' => $model]);
-    }
-    
-    public function actionListar_solcitudes_servicio() {                      //renderiza el index de la carpeta agencia dentro de views
-        $model = new ListaSolicitudesServicioModel();
-        return $this->render("listarSolicitudes", ['model' => $model]);
-    }
-    
-    
 
 }
