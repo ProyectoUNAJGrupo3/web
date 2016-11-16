e28ca9ca980e9e584a9c4781943dc48e19c75e11
MarceloMansilla <mansillamarcelo90@gmail.com>
2016-11-16 18:20:42 -0300

    Agregando logo y haciendo unas correciones en la vistas registrar Usuario y Agencia. Arreglando redireccionamiento en en el layout de Recepcionista

--- mvc/views/layouts/mainAgencia.php
+++ mvc/views/layouts/mainAgencia.php
@@ -36,7 +36,7 @@ $this->title = 'Agencia';
             <?php
             NavBar::begin([
                 'id' => 'barra-agencia',
-                'brandLabel' => '<img src="img/logo.ico" style="display:inline; margin-top: -15px; vertical-align: top; width:50px; height:50px;">&nbsp&nbsp&nbsp&nbsp<b styel="size:15px">Agencia</b>',
+                'brandLabel' => '<img src="img/LogoApp.png" style="display:inline; margin-top: -20px; vertical-align: top; width:120px; height:55px;">&nbsp&nbsp&nbsp&nbsp<b styel="size:15px">Agencia</b>',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
