5a1cb00f25c0cc0213bd8844f71653cbe49273fe
Mendez Alejandro Ezequiel <mendezalejandro.e@gmail.com>
2016-12-01 12:58:34 -0300

    Opcion Actualizar tarifa funcionando. ABM Clientes completo y funcionando.

--- mvc/models/Recepcionista/ActualizarTarifaModel.php
+++ mvc/models/Recepcionista/ActualizarTarifaModel.php
@@ -4,7 +4,55 @@ namespace app\models\Recepcionista;
 
 use yii;
 use yii\base\Model;
+use app\models\CapaServicio\TarifasModelo;
 
 class ActualizarTarifaModel extends Model {
-    
+    public $AgenciaID;
+    public $TarifaID;
+    public $Comision;
+    public $PrecioKM;
+    public $KmMinimo;
+    public $ViajeMinimo;
+    public $Estado;
+    public $actualizar_TarifaError;
+    public function rules() {
+        return[
+            //([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+            //(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
+            ['PrecioKM', 'required', 'message' => 'Campo obligatorio'],
+            ['KmMinimo', 'required', 'message' => 'Campo obligatorio'],
+            ['ViajeMinimo', 'required', 'message' => 'Campo obligatorio'],
+            ['Estado', 'required', 'message' => 'Campo obligatorio'],
+            ['Comision', 'string'],
+            ['Estado', 'string'],
+        ];
+    }
+    public function actualizarTarifa($TarifaID)
+    {
+        $this->AgenciaID = Yii::$app->user->identity->AgenciaID;
+        $model = new TarifasModelo();
+        try {
+            $tarifaModificada = $model->ModificarTarifa($TarifaID,$this->Comision,$this->AgenciaID,$this->ViajeMinimo,$this->KmMinimo,$this->PrecioKM,$this->Estado);
+            $tarifaModificada = array_shift($tarifaModificada);
+            if (!is_null($tarifaModificada['_Result']))
+            {
+                return true;
+            }
+            else return false;
+        }
+        catch (\yii\db\Exception $e) {
+            $messageError = $e->errorInfo[2];
+            $this->addError('actualizar_TarifaError',$messageError);
+            return false;
+        }
+
+    }
+    public function setAtributos($tarifaSelected)
+    {
+        $this->PrecioKM=$tarifaSelected['PrecioKM'];
+        $this->KmMinimo=$tarifaSelected['KmMinimo'];
+        $this->ViajeMinimo=$tarifaSelected['ViajeMinimo'];
+        $this->Estado=$tarifaSelected['Estado'];
+        $this->Comision=$tarifaSelected['Comision'];
+    }
 }
--- mvc/views/recepcionista/administrarCliente.php
+++ mvc/views/recepcionista/administrarCliente.php
@@ -3,8 +3,7 @@
 use yii\helpers\BaseHtml;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use app\assets\AppAsset;
-use app\assets\AppAssetAgencia;
+use app\assets\AppAssetRecepcionista;
 use yii\grid\GridView;
 use yii\helpers\ArrayHelper;
 use yii\bootstrap\Modal;
@@ -14,57 +13,105 @@ use yii\bootstrap\Alert;
 use app\assets\BootswatchAsset;
 
 raoul2000\bootswatch\BootswatchAsset::$theme = 'superhero';
-BootswatchAsset::register($this);
-//AppAssetAgencia::register($this);
-//AppAsset::register($this);
+AppAssetRecepcionista::register($this);
+
+?>
+<?php
 Modal::begin([
-    'id' => 'modal',
-    //'size' => 'modal-lg',
+    'id' => 'cliente_modal',
+    'size' => 'modal-lg',
 ]);
 echo "<div id='modalContent'></div>";
 Modal::end();
 ?>
 <div class="panel panel-primary">
     <div class="panel-heading">
-        <h4 class="panel-title">Administrar Cliente</h4>
+        <h4 class="panel-title">Administrar Clientes</h4>
     </div>
     <div class="panel-body">
 
-<?php Pjax::begin(['id' => 'solicitudes_containerpjax', 'timeout' => false]); ?>
-        <?php if (Yii::$app->session->hasFlash('solicitudAutorizada')): ?>
+<?php Pjax::begin(['id' => 'clientes_containerpjax', 'timeout' => false]); ?>
+        <?php if (Yii::$app->session->hasFlash('clienteCreado')): ?>
             <div class="alert alert-success alert-dismissable">
                 <button aria-hidden="true" data-dismiss="alert" class="close" type="button">X</button>
                 <h4>
                     <i class="icon fa fa-check"></i>Operacion realizada.
                 </h4>
-    <?= Yii::$app->session->getFlash('solicitudAutorizada') ?>
+                <?= Yii::$app->session->getFlash('clienteCreado') ?>
             </div>
             <?php endif; ?>
-        <?php if (Yii::$app->session->hasFlash('solicitudRechazada')): ?>
-            <div class="alert alert-success alert-dismissable">
+        <?php if (Yii::$app->session->hasFlash('agregar_ClienteError')): ?>
+            <div class="alert alert-danger alert-dismissable">
                 <button aria-hidden="true" data-dismiss="alert" class="close" type="button">X</button>
                 <h4>
-                    <i class="icon fa fa-check"></i>Operacion realizada.
+                    <i class="icon fa fa-check"></i>Operacion cancelada.
                 </h4>
-    <?= Yii::$app->session->getFlash('solicitudRechazada') ?>
+                <?= Yii::$app->session->getFlash('agregar_ClienteError') ?>
             </div>
             <?php endif; ?>
+        <?php if (Yii::$app->session->hasFlash('clienteEliminado')): ?>
+        <div class="alert alert-success alert-dismissable">
+            <button aria-hidden="true" data-dismiss="alert" class="close" type="button">X</button>
+            <h4>
+                <i class="icon fa fa-check"></i>Operacion realizada.
+            </h4>
+            <?= Yii::$app->session->getFlash('clienteEliminado') ?>
+        </div>
+        <?php endif; ?>
+        <?php if (Yii::$app->session->hasFlash('eliminar_ClienteError')): ?>
+        <div class="alert alert-danger alert-dismissable">
+            <button aria-hidden="true" data-dismiss="alert" class="close" type="button">X</button>
+            <h4>
+                <i class="icon fa fa-check"></i>Operacion cancelada.
+            </h4>
+            <?= Yii::$app->session->getFlash('eliminar_ClienteError') ?>
+        </div>
+        <?php endif; ?>
+        <?php if (Yii::$app->session->hasFlash('clienteActualizado')): ?>
+        <div class="alert alert-success alert-dismissable">
+            <button aria-hidden="true" data-dismiss="alert" class="close" type="button">X</button>
+            <h4>
+                <i class="icon fa fa-check"></i>Operacion realizada.
+            </h4>
+            <?= Yii::$app->session->getFlash('clienteActualizado') ?>
+        </div>
+        <?php endif; ?>
+        <?php if (Yii::$app->session->hasFlash('actualizar_ClienteError')): ?>
+        <div class="alert alert-danger alert-dismissable">
+            <button aria-hidden="true" data-dismiss="alert" class="close" type="button">X</button>
+            <h4>
+                <i class="icon fa fa-check"></i>Operacion cancelada.
+            </h4>
+            <?= Yii::$app->session->getFlash('actualizar_ClienteError') ?>
+        </div>
+        <?php endif; ?>
         <?php $form = ActiveForm::begin(['options' => ['data-pjax' => true]]); ?>
         <div class="table-responsive">
-
+            <?=
+            GridView::widget([
+            'id' => 'clientes_grid',
+            'summary'=>'',
+            'dataProvider' => $model->dataProvider,
+            'tableOptions' => ['class' => 'table table-bordered table-hover', 'style'=>'border-collapse: collapse; border: 3px solid #df691a; '],
+            'columns' => [
+                ['header' => '<h5>Nombre</h5>','attribute' => 'Nombre','contentOptions' => ['style'=>'border-color:black;'],'headerOptions' => ['style'=>'border-color:black;background-color:#df691a;']],
+                ['header' => '<h5>Apellido</h5>','attribute' => 'Apellido','contentOptions' => ['style'=>'border-color:black;'],'headerOptions' => ['style'=>'border-color:black;background-color:#df691a;']],
+                ['header' => '<h5>Documento</h5>','attribute' => 'Documento','contentOptions' => ['style'=>'border-color:black;'],'headerOptions' => ['style'=>'border-color:black;background-color:#df691a;']],
+                ['header' => '<h5>Telefono</h5>','attribute' => 'Telefono','contentOptions' => ['style'=>'border-color:black;'],'headerOptions' => ['style'=>'border-color:black;background-color:#df691a;']],
+                ['header' => '<h5>Email</h5>','attribute' => 'Email','contentOptions' => ['style'=>'border-color:black;'],'headerOptions' => ['style'=>'border-color:black;background-color:#df691a;']],
+            ],
+            'rowOptions' => function ($model, $key, $index, $grid) {
+                return ['rowid' => $key, 'onclick' => '$(this).addClass("success").siblings().removeClass("success");','style' => 'cursor:pointer'];
+            },
+      ]);
+            ?>
         </div>
-
-<?php ActiveForm::end(); ?>
+        <?php ActiveForm::end(); ?>
         <?php Pjax::end(); ?>
-        <div id="solicitudesbuttonsOperaciones">
-
-<?= Html::button('Agregar', ['value' => Url::toRoute('#'), 'class' => 'btn btn-primary btn-lg', 'id' => 'modalButton']); ?>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <?= Html::Button('Actualizar', ['value' => Url::toRoute('#'), 'valor' => 'actualizar', 'name' => 'submit', 'class' => 'btn btn-primary btn-lg', 'id' => 'actualizarButton']); ?>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <?= Html::Button('Eliminar', ['class' => 'btn btn-primary btn-lg', 'name' => 'submit', 'valor' => 'eliminar', 'id' => 'eliminarButton']); ?>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <?= Html::a('Cerrar', [('/recepcionista/index')], ['class' => 'btn btn-primary btn-lg', 'id' => 'btn-cancelar']); ?>
+        <div id="clientesbuttonsOperaciones">
+            <?= Html::button('Agregar',['value' => Url::toRoute('recepcionista/alta_cliente'), 'class' => 'btn btn-lg btn-primary', 'id' => 'cliente_agregarButton','operacion'=>'agregar',]);?>
+            <?= Html::button('Modificar',['value' => Url::toRoute('recepcionista/actualizar_cliente'), 'class' => 'btn btn-lg btn-primary', 'id' => 'cliente_actualizarButton','operacion'=>'actualizar',]);?>
+            <?= Html::button('Eliminar',['value' => Url::toRoute('recepcionista/eliminar_cliente'), 'class' => 'btn btn-lg btn-primary', 'id' => 'cliente_eliminarButton','operacion'=>'eliminar',]);?>
         </div>
     </div>
 </div>
