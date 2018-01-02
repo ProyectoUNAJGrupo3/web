6e23781aef0695eae2f11eb86d5ac7659b45a910
mendezalejandro <mendezalejandro.e@gmail.com>
2018-01-02 10:54:15 -0300

    Update db.php
    Se cambia la BD que estaba en el servidor de Nelson por la BD alojada en Azure

--- mvc/config/db.php
+++ mvc/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.99.203.134;dbname=unaj_proyecto',
-    'username' => 'unaj_app',
-    'password' => 'u79l2vak9wh5AZ3219',
+    'dsn' => 'mysql:host=br-cdbr-azure-south-b.cloudapp.net;dbname=remisya_dev',
+    'username' => 'ba34f0f1e45619',
+    'password' => '32ed9725',
     'charset' => 'utf8',
 ];
